431b7c251cac383647f56a1b147c91ced081701d
abarhub <tulipebleu1@laposte.net>
2025-11-29 18:33:21 +0100

    concervation de l'ordre des commandes

--- src/main/java/org/jsystray/Main.java
+++ src/main/java/org/jsystray/Main.java
@@ -1,13 +1,17 @@
 package org.jsystray;
 
+import java.util.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Objects;
-import java.util.Properties;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -19,21 +23,7 @@ public class Main {
 
         try {
             // Load properties from resources
-            Properties props = new Properties();
-            boolean configTrouve = false;
-            if (args.length > 1 && Objects.equals(args[0], "--config")) {
-                String configFile = args[1];
-                try (InputStream input = Files.newInputStream(Path.of(configFile))) {
-                    props.load(input);
-                    configTrouve = true;
-                }
-            }
-
-            if (!configTrouve) {
-                try (InputStream input = Main.class.getClassLoader().getResourceAsStream("commands.properties")) {
-                    props.load(input);
-                }
-            }
+            Map<String, String> props = loadProperties(args);
 
             // Load tray icon image
             Image image = Toolkit.getDefaultToolkit().getImage(Main.class.getClassLoader().getResource("tray_icon.png"));
@@ -41,8 +31,8 @@ public class Main {
             // Create popup menu
             PopupMenu popup = new PopupMenu();
 
-            for (String key : props.stringPropertyNames()) {
-                String command = props.getProperty(key);
+            for (String key : props.keySet()) {
+                String command = props.get(key);
                 MenuItem item = new MenuItem(key);
                 item.addActionListener((ActionEvent e) -> runCommand(command));
                 popup.add(item);
@@ -64,6 +54,23 @@ public class Main {
         }
     }
 
+    private static Map<String, String> loadProperties(String[] args) throws IOException {
+        boolean configTrouve = false;
+        if (args.length > 1 && Objects.equals(args[0], "--config")) {
+            String configFile = args[1];
+            try (InputStream input = Files.newInputStream(Path.of(configFile))) {
+                return loadProperties(input);
+            }
+        }
+
+        if (!configTrouve) {
+            try (InputStream input = Main.class.getClassLoader().getResourceAsStream("commands.properties")) {
+                return loadProperties(input);
+            }
+        }
+        return Map.of();
+    }
+
     private static void runCommand(String command) {
         try {
             String[] tab = SplitUtils.split(command);
@@ -73,4 +80,32 @@ public class Main {
         }
     }
 
+    private static Map<String, String> loadProperties(InputStream input) {
+        var buffer = new BufferedReader(
+                new InputStreamReader(input, StandardCharsets.UTF_8));
+        try (var lines = buffer.lines()) {
+            return lines
+                    .filter(x -> x != null && !x.isBlank())
+                    .filter(x -> x.trim().startsWith("#"))
+                    .map(Main::split)
+                    .filter(x -> x.size() != 2)
+                    .collect(Collectors.toMap(List::getFirst,
+                            List::getLast,
+                            (e1, e2) -> {
+                                throw new RuntimeException("Same key : " + e1);
+                            },
+                            LinkedHashMap::new));
+        }
+    }
+
+    private static List<String> split(String s) {
+        if (s == null || !s.contains("=")) {
+            return List.of();
+        }
+        var pos = s.indexOf("=");
+        if (pos == -1) {
+            return List.of();
+        }
+        return List.of(s.substring(0, pos), s.substring(pos + 1));
+    }
 }
